c03fae806db1b8b2490a826e972b01406b0baafa
Eric Kim <kimericbyung@gmail.com>
2021-03-02 13:28:59 -0800

    Completed 3sum

---
+++ 3sum.js
@@ -0,0 +1,39 @@
+const threeSum = nums => {
+  if (nums.length < 3) {
+    return [];
+  }
+  let sums = [];
+  let left;
+  let right;
+  let sum;
+  nums = nums.sort((a, b) => a - b)
+  for (let i = 0; i < nums.length; i++) {
+    if (nums[i] === nums[i - 1]) {
+      continue;
+    }
+    left = i + 1;
+    right = nums.length - 1;
+    while (left < right) {
+      sum = nums[i] + nums[left] + nums[right];
+      if (sum < 0) {
+        left++;
+      } else if (sum > 0) {
+        right--;
+      } else {
+        if (nums[right] === nums[right + 1]) {
+          right--;
+          continue;
+        }
+        sums.push([nums[i], nums[left], nums[right]]);
+        left++;
+        right--;
+      }
+    }
+  }
+  return sums;
+};
+
+console.log(threeSum([-1,0,1,2,-1,-4]));
+// console.log(threeSum([1,2,-2,-1]));
+console.log(threeSum([1,-1,-1,0]))
+// -1 -1 0 1
